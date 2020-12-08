eedb410a263317800372d10520b00716fd306625
Tomasz <tom@Tomaszs-MacBook-Pro.local>
2020-12-08 21:50:32 +0000

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Interfaces;
-using Infrastructure;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
