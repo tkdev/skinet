957ee4877322bc6e64a7c71b0d92599f54716958
Tom <tom.krut@gmail.com>
2021-11-13 21:31:52 +0000

    Move consuming http services to shop component (shop module, component, service)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './shared/models/product';
-import { IPagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +8,10 @@ import { IPagination } from './shared/models/pagination';
 
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: any[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    })
   }
   
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IPagination } from '../shared/models/pagination';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseURL = 'https://localhost:5001/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get<IPagination>(this.baseURL + 'products?pageSize=50');
+  }
 }
