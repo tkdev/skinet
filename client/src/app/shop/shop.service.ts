06802457fe6be94d7988002c35e9cf0ed97332f3
Tom <tom.krut@gmail.com>
2021-11-15 12:16:15 +0000

    add service to get product brands and types from api and populate filters on the page

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
+import { IType } from '../shared/models/productType';
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +15,13 @@ export class ShopService {
   getProducts() {
     return this.http.get<IPagination>(this.baseURL + 'products?pageSize=50');
   }
+
+  getBrands() {
+    return this.http.get<IBrand[]>(this.baseURL + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<IType[]>(this.baseURL + 'products/types');
+  }
+
 }
